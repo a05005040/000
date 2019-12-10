6bf90c96028fbc2a4756d0b7ed0a6bd2acc8c9ec
a05005040 <55218579+a05005040@users.noreply.github.com>
2019-12-10 13:34:28 +0800

    1210

--- 0619/Assets/程式/ItemManager.cs
+++ 0619/Assets/程式/ItemManager.cs
@@ -15,14 +15,18 @@ public class ItemManager : MonoBehaviour
     {
         "棒棒糖", "飽食", "吐司", "髮圈"
     };
+    [Header("道具價錢")]
+    public int[] spice = { 100, 200, 250, 300 };
 
-    public GameObject UIBuy;
+   
+
+    public GameObject UIBuy, UI_MessageBox;
     public Text TextTip;
     public Text Text_01, Text_02, Text_03, Text_04;
 
     private void Start()
     {
-       
+        Set_mineceraft();
     }
 
     private void Update()
@@ -33,6 +37,7 @@ public class ItemManager : MonoBehaviour
         Text_04.GetComponent<Text>().text = ItemsCount[3].ToString();
 
         Get_minecraft.GetComponent<Text>().text = minecraft_scroce.ToString();   //藥水
+         Set_mineceraft();
     }
     public void SetIndex(int index)
     {
@@ -45,14 +50,32 @@ public class ItemManager : MonoBehaviour
 
     public void Buy()
     {
-        ItemsCount[Index]++;
         UIBuy.SetActive(false);
+        if (minecraft_scroce < spice.Length)
+        {
+            UI_MessageBox.SetActive(true);
+            return;
+        }
+        ItemsCount[Index]++;
+       
+        
+        minecraft_scroce -= spice[Index];
+
     }
 
     public void Cancel()
     {
         UIBuy.SetActive(false);
-    } 
+    }
+    /// <summary>
+    /// 摩法藥水不足提示窗
+    /// </summary>
+    public void insufficient()
+    {
+      
+        UI_MessageBox.SetActive(false);
+
+    }
 
 
     /// <summary>
@@ -63,6 +86,6 @@ public class ItemManager : MonoBehaviour
 
         minecraft_scroce = minecraft_scroce + Catch_minecraft.Score_Num;
         Get_minecraft.text = minecraft_scroce.ToString();
-
+        
     }
 }
